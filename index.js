784fecf99b61ca2a5895922bc4695d5698d66e7c
bailin240 <bailin240@163.com>
2019-04-03 14:55:44 +0800

    算法学习

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 "use strict"
 
-
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+let nums = [
+    [1, 2],
+    [2, 2]
+]
+var isToeplitzMatrix = function (matrix) {
+    for (let i = 1; i < matrix.length; i++) {
+        let item = matrix[i]
+        for (let j = 1; j < item.length; j++) {
+            let ite = item[j]
+            if (matrix[i - 1][j - 1] != ite) {
+                return false
+            }
+        }
+    }
+    return true
+};
 
 
 console.time()
 
-console.log(majorityElement(nums))
+console.log(isToeplitzMatrix(nums))
 
 console.timeEnd()
